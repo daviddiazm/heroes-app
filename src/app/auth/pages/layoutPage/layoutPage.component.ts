2eab6b08ea28309dda5cc34e13231651f5b31b3c
david <davidcol123dm@gmail.com>
2024-07-22 19:45:03 -0500

    layouts y childrens sin lazyload

---
+++ src/app/auth/pages/layoutPage/layoutPage.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-layout-page',
+  templateUrl: './layoutPage.component.html',
+  styles: `
+    :host {
+      display: block;
+    }
+  `
+})
+export class LayoutPageComponent { }
